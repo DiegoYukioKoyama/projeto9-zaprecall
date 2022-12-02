b7c620a0d2d1a880e0926da98ba56520ede712f7
DiegoKoyama <yukioko.jp@gmail.com>
2022-12-02 10:05:58 -0300

    Component Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import logo from "../assets/img/logo.png"
+
+function Logo() {
+    return (
+
+        <div className="logo-container">
+            <img src={logo} alt="logo" />
+            <h1>ZapRecall</h1>
+        </div>
+
+    )
+}
+
+export default Logo
